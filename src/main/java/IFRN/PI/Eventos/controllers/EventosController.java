1236983a6e106df72f2a0908ab8f8cac083426a2
Hugo Chinnaid da Silva Caxias <20211204010012@ifrn.local>
2024-10-31 14:28:21 -0300

    Enviando dados do formulário para a aplicação

--- src/main/java/IFRN/PI/Eventos/controllers/EventosController.java
+++ src/main/java/IFRN/PI/Eventos/controllers/EventosController.java
@@ -1,7 +1,11 @@
 package IFRN.PI.Eventos.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import IFRN.PI.Eventos.models.Evento;
+//import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class EventosController {
@@ -9,6 +13,14 @@ public class EventosController {
 	@RequestMapping("/eventos/form")
 	public String form() {
 		return "formEvento";
+	}
+	@PostMapping("/eventos")
+	//@RequestMapping(path = "/eventos", method = RequestMethod.POST )
+	public String adicionar(Evento evento) {
+		
+	System.out.println(evento);
+	
+		return "evento-adicionado";
 	}
 
 }
